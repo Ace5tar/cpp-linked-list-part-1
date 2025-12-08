a03d31f65954c43fc595f9827ff4f0f9e8096109
Ace5tar <tace233@gmail.com>
2025-12-08 14:03:34 -0800

    implemented example of how node works in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "Node.h"
+#include "Student.h"
 
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+
+	Node* head = new Node();
+	head->setNext(new Node());
+	head->setStudent(new Student("Name", 123));
+	head->getNext()->setStudent(new Student("Name2", 456));
+
+	cout << "Student at index zero name: " << head->getStudent()->getName() << endl; 
+	cout << "Student at index zero name: " << head->getStudent()->getId() << endl; 
+	cout << "Student at index one name: " << head->getNext()->getStudent()->getName() << endl; 
+	cout << "Student at index one name: " << head->getNext()->getStudent()->getId() << endl; 
+	
+	delete head;
+
 	return 1;
 }
